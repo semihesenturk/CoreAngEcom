5f02b9837e798f5972bde1440c2a9d7da055d5dd
semih.esenturk <semihesenturk@outlook.com>
2022-04-04 17:52:05 +0300

    db operations an migrations has added

---
+++ Infrastructure/CoreAngEcom.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using CoreAngEcom.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CoreAngEcom.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CoreAngEcomDbContext>
+    {
+        public CoreAngEcomDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<CoreAngEcomDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
+
